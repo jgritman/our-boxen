1aacea0212027004b2b98892bbf2423b24b3371e
Jason Gritman <jasongritman@gmail.com>
2014-12-14 18:46:38 -0700

    Jason's clean mac setup

--- manifests/site.pp
+++ manifests/site.pp
@@ -58,6 +58,48 @@ node default {
   include hub
   include nginx
 
+  #jason's stuff
+  include alfred
+  include bartender
+  include chrome
+  # include cloudapp
+  include dash
+  include dropbox
+  include eclipse::ggts
+  include evernote
+  include firefox
+  include github_for_mac
+  include hipchat
+  include inconsolata  
+  include istatmenus4
+  include iterm2::stable
+  include java
+  include kindle
+  include mailbox
+  include mongodb
+  include mysql
+  include ohmyzsh
+  include onepassword
+  include skype
+  include sourcetree
+  include spotify
+  include sublime_text
+  include virtualbox
+  include vlc
+  include wget
+  include zsh
+
+  class { 'vagrant': }
+
+  include osx::global::disable_remote_control_ir_receiver
+  include osx::global::disable_autocorrect
+  include osx::finder::unhide_library
+  include osx::finder::show_hidden_files
+  include osx::finder::show_all_filename_extensions
+  include osx::safari::enable_developer_mode
+  include osx::no_network_dsstores
+  include osx::software_update
+
   # fail if FDE is not enabled
   if $::root_encrypted == 'no' {
     fail('Please enable full disk encryption and try again')
@@ -74,6 +116,10 @@ node default {
   ruby::version { '2.1.0': }
   ruby::version { '2.1.1': }
   ruby::version { '2.1.2': }
+
+  sublime_text::package { 'Emmet':
+    source => 'sergeche/emmet-sublime'
+  }
 
   # common, useful packages
   package {
